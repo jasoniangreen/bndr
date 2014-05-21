ef98440360d7daea94d4cf536d544efba64f5e82
Jason Green <jasoniangreen@gmail.com>
2014-05-21 20:09:20 +0100

    Added clocks example and component names

--- GruntFile.js
+++ GruntFile.js
@@ -1,13 +1,20 @@
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
+        browserify: {
+            bndr: {
+                files: {
+                    'dist/bndr.bundle.js': 'lib/bndr.js'
+                }
+            }
+        },
         watch: {
             js: {
                 files: [
-                    'lib/*.js',
+                    'lib/**/*.js',
                     'Gruntfile.js'
                 ],
-                tasks: ['uglify']
+                tasks: ['browserify']
             }
         },
         uglify: {
@@ -16,16 +23,17 @@ module.exports = function(grunt) {
             },
             js: {
                 files: {
-                    'dist/bndr.min.js': 'lib/bndr.js'
+                    'dist/bndr.min.js': 'dist/bndr.bundle.js'
                 }
             }
         },
     });
 
     // Load the Grunt plugins.
+    grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Register the default tasks.
-    grunt.registerTask('default', ['uglify', 'watch']);
+    grunt.registerTask('default', ['browserify', 'uglify', 'watch']);
 };
